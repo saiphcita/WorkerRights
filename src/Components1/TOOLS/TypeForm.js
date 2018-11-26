7c38c5f51e32959ff05059cba0e560a18934d98b
oscar orellana <oscar.orellana.mon@gmail.com>
2018-11-25 20:56:21 -0600

    adding tittle ad quit button mobile

--- src/Components1/TOOLS/TypeForm.js
+++ src/Components1/TOOLS/TypeForm.js
@@ -13,6 +13,7 @@ class Form extends React.Component {
       typeformEmbed.makeWidget(this.el, "https://oscarorellana1.typeform.com/to/fKu7SZ", {
         hideFooter: true,
         hideHeaders: true,
+        buttonText: false,
         hideScrollbars: true,
         opacity: 100
       });
@@ -22,6 +23,7 @@ class Form extends React.Component {
     return (
         <div style={{width: '100%', height: '100%'}}>
             <Link to="/Graph" className="linkG">Ir a las Gráficas</Link>
+            <h1 style={{margin:"0", padding:"0", textAlign:"center", color: "#EFDB49", backgroundColor:"#39A2A3"}}>Especificaciones de Nuevo Contrato Público </h1>
             <div ref={(el) => this.el = el} style={{width: '100%', height: '100%'}} />
         </div>
     )
